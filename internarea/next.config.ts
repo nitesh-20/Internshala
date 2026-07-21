9620d42d489c9829efc392f2498ca0383e5c4b88
nitesh-20 <nsahu6156@gmail.com>
2026-07-21 16:06:11 +0530

    Fix Vercel build: Hardcode Firebase env vars directly in next.config.ts

--- internarea/next.config.ts
+++ internarea/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyB8VMfxTNXpVEEZo2sxJ2gRk-O2nVzatSI",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "internshala-b0d41.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "internshala-b0d41",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "internshala-b0d41.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "509604426013",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:509604426013:web:387cff0acc962096b331f4",
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-V5KTVJ249V",
+  },
 };
 
 export default nextConfig;
