0b3060dfe4b0d6b0d042388ad3fa33ae2bb8a664
nitesh-20 <nsahu6156@gmail.com>
2026-07-22 13:37:11 +0530

    refactor: update API base URL for post internship page

--- internarea/src/pages/postInternship/index.tsx
+++ internarea/src/pages/postInternship/index.tsx
@@ -47,7 +47,7 @@ const index = () => {
     }
     try {
       setisloading(true);
-      const res = await axios.post("http://localhost:5001/api/internship", formData);
+      const res = await axios.post("https://backend-tau-snowy-58.vercel.app/api/internship", formData);
       toast.success("job posted successfuly");
       router.push("/adminpanel");
     } catch (error) {
